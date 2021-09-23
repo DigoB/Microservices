3e70ec5617b9e857288530a38040c7b009fd6945
Rodrigo Braz <braz.rodrigo94@gmail.com>
2021-09-22 22:07:31 -0300

    Configuring Zuul Api Gateway

---
+++ hr-api-gateway-zuul/src/test/java/br/com/rodrigobraz/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigobraz.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
