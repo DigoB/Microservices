2197af2682dacccc06241127e073450aa0ccc7e8
Rodrigo Braz <braz.rodrigo94@gmail.com>
2021-09-22 23:36:41 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/rodrigobraz/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/rodrigobraz/hrworker/controllers/WorkerController.java
@@ -5,6 +5,7 @@ import com.rodrigobraz.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,21 @@ public class WorkerController {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private WorkerRepository repository;
 
     @Autowired
     private Environment env;
+
+    @GetMapping("/configs")
+    public ResponseEntity<?> getConfigs() {
+
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> listAll() {
@@ -35,14 +46,6 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
-
-        /*
-        try {
-            Thread.sleep(3000L);   
-        } catch (Exception e) {
-            e.printStackTrace();
-        } 
-        */
 
         logger.info("Searching for worker by id: " + id);
         logger.info("PORT = " + env.getProperty("local.server.port"));
